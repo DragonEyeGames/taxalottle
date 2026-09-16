0dda6e489f45ea1b5426b75d3fbc532e247f0cae
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2026-09-16 13:59:29 -0400

    Images!
    I was able to display an image with Java and CSS! We are moving further on our game! I didn't feel like working on the main section of my website today so this is what i did.

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import { resources } from './resource.js';
+
+const canvas = document.querySelector("#game-canvas");
+const ctx = canvas.getContext("2d");
+
+const draw = () => {
+    const sky= resources.images.sky;
+    if (sky.isLoaded) {
+        ctx.drawImage(sky.image, 0, 0)
+    }
+}
+setInterval(() => {
+    console.log("draw")
+    draw()
+}, 300)
